d0d210e4e319dcb77eb3567cef7aaeeb0560b82e
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2022-02-16 13:57:23 +0200

    Time: 60 ms (14.56%), Space: 13.9 MB (76.56%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,6 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        while "()" in s or "[]" in s or "{}" in s:
-            s=s.replace("()","").replace("[]","").replace("{}","")
-        return s==''
+        hasmap = {')' : '(', ']' : '[', '}' : '{'}
+        stack = []
+        for st in s : 
+            if st in ['(', '[', '{'] : 
+                stack.append(st)
+            elif stack : 
+                item = stack.pop()
+                if item != hasmap[st]  : 
+                    return False
+            else : 
+                return False
+        
+        if not stack : 
+            return True
+        else : 
+            return False
 
