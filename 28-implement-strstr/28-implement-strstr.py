35188459bcac9f5567ca5dcb2218026ce2a91f01
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2022-02-17 13:45:07 +0200

    Time: 55 ms (60.30%), Space: 14.3 MB (69.69%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -6,4 +6,8 @@ class Solution:
             return -1
         if not needle:
             return 0
+        if len(needle) > len(haystack):
+            return -1
+        if needle[0] not in haystack:
+            return -1
         return haystack.find(needle)
