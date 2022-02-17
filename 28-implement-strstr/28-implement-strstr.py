2e65a60b5985c4bb30f72d6b749384ed4c0ca832
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2022-02-17 13:48:45 +0200

    Time: 32 ms (93.07%), Space: 14.1 MB (86.30%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -2,8 +2,8 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if not haystack and not needle:
             return 0
-        if not haystack:
-            return -1
+        #if not haystack:
+         #   return -1
         if needle not in haystack:
             return -1
         if needle in haystack:
