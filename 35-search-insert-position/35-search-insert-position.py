8e3b168227d66d21c555e8efecb6800c0eaaca1d
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2022-02-18 13:47:14 +0200

    Time: 194 ms (5.05%), Space: 14.8 MB (65.58%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in nums:
+            if i == target:
+                return nums.index(i)
+            if target not in nums:
+                if target > nums[-1]:
+                    diff = target - nums[-1]
+                    il = nums.index(nums[-1])
+                    hp = il+1
+                    return hp
+                elif i < target:
+                    continue
+                elif i > target:
+                    ip = nums.index(i)
+                    hip = ip
+                    return hip
+                    break
