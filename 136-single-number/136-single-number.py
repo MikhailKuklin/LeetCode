35e8b0341c9f32ac83a63dafa7d5cee1cf459a13
Mikhail Kuklin <57541345+MikhailKuklin@users.noreply.github.com>
2022-02-20 14:26:47 +0200

    Time: 136 ms (84.36%), Space: 16.8 MB (13.91%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -4,12 +4,3 @@ class Solution:
         for i in nums:
             a = i^a
         return a
-        """
-        u = []
-        for i in nums:
-            if i not in u:
-                u.append(i)
-            elif i in u:
-                u.remove(i)
-        return u[0]
-        """
